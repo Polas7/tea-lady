fec396dca0581bdff689ea2c644951ea5964c5b3
Pola <Pola.kinomoto@gmail.com>
2017-05-12 12:21:22 -0300

    adding files

---
+++ js/main.js
@@ -0,0 +1,10 @@
+
+ 
+function datosCliente(nombre,apellido,email,direccion, telefono){
+	this.nombre=nombre;
+	this.apellido=apellido;
+	this.email=email;
+	this.direccion=direccion;
+	this.telefono=telefono;
+}
+
