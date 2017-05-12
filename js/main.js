145cf2ef7c686d8a691753907779b7a5d5d98be6
Pola <Pola.kinomoto@gmail.com>
2017-05-12 13:14:56 -0300

    agregra funcion mostrar data

--- js/main.js
+++ js/main.js
@@ -10,4 +10,18 @@ function datosCliente(nombre,apellido,email,direccion, telefono){
 	this.direccion=direccion;
 	this.telefono=telefono;
 }
+function mostrarData(){
+var nombre= document.getElementById("Nombre").value
+var apellido= document.getElementById("Apellido").value
+var email= document.getElementById("Email").value
+var direccion= document.getElementById("Direccion").value
+var telefono= document.getElementById("Telefono").value	
+var cliente= new datosCliente(nombre, apellido, email, direccion, telefono);
+var compras= [];
+compras.push(cliente);
 
+compras.forEach(function (elements){
+var mostrar = document.getElementById("clientes")
+mostrar.innerHTML+= elements.nombre + elements.apellido + elements.email + elements.direccion + elements.telefono
+});
+}
